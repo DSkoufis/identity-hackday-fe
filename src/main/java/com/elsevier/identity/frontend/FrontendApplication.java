875f812bd3f16892f879d121839a147958043162
DSkoufis <skoufisdimitris@gmail.com>
2020-02-25 10:50:34 +0000

    Init commit

---
+++ src/main/java/com/elsevier/identity/frontend/FrontendApplication.java
@@ -0,0 +1,13 @@
+package com.elsevier.identity.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendApplication.class, args);
+	}
+
+}
